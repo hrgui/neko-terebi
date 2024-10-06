b5247283864252e4d82df96598dd3eb3a3cb9b01
Harman Goei <5001432+hrgui@users.noreply.github.com>
2024-10-05 22:04:36 -0700

    feat: setup tailwind in storybook, add story

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,16 @@
 import type { Preview } from "@storybook/react";
+import "../src/app/index.css";
 
 const preview: Preview = {
+  initialGlobals: {
+    // 👇 Set the initial background color
+    backgrounds: { value: "dark" },
+  },
   parameters: {
+    backgrounds: {
+      values: [{ name: "Dark", value: "#000" }],
+      default: "Dark",
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
