1f632c991bddb250817e05a934ef8705b6e1b578
Harman Goei <5001432+hrgui@users.noreply.github.com>
2024-11-01 01:08:14 -0700

    feat: add expected Story for Cell

--- packages/components/Cell/Cell.stories.tsx
+++ packages/components/Cell/Cell.stories.tsx
@@ -7,3 +7,12 @@ export default meta;
 type Story = StoryObj<typeof Cell>;
 
 export const Default: Story = {};
+
+export const BocchiTheRock: Story = {
+  args: {
+    imageUrl: "https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=1200,height=675/catalog/crunchyroll/88fbe563405097ee6eeeccd9d46195e2.jpg",
+    header1: "BOCCHI THE ROCK!",
+    header2: "Sub | Dub",
+    header3: "14+"
+  }
+};
--- packages/components/Cell/Cell.tsx
+++ packages/components/Cell/Cell.tsx
@@ -2,18 +2,43 @@ import { twMerge } from "tailwind-merge";
 
 export type Props = {
   focused?: boolean;
+  header1?: string;
+  header2?: string;
+  header3?: string;
+  imageUrl?: string;
 };
 
-const Cell = ({ focused }: Props) => {
+const Cell = ({ focused, header1, header2, header3, imageUrl }: Props) => {
   return (
     <div className="@asvw:w-[528px]">
       <div
         className={twMerge(
-          "w-full @asvw:h-[297px] bg-gray-500/100 @asvw:rounded-[26.4px]",
+          "w-full @asvw:h-[297px] bg-gray-700/100 @asvw:rounded-[26.4px]",
           focused ? "outline outline-white  @asvw:outline-[10px]" : ""
         )}
+        style={{
+          backgroundImage: `url("${imageUrl}")`,
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
       ></div>
-      <div className="w-full @asvw:h-[171px] bg-gray-200 @asvw:mt-[24px]"></div>
+      <div className="w-full @asvw:h-[171px] @asvw:mt-[24px]">
+        <div
+          className={`${twMerge(
+            "font-semibold text-gray-400/100",
+            focused && "text-gray-200/100"
+          )} text-headline-s`}
+        >
+          {header1}
+        </div>
+        <div className={twMerge("font-semibold text-gray-500/100", focused && "text-gray-300/100")}>
+          {header2}
+        </div>
+        <div className={twMerge("font-semibold text-gray-500/100", focused && "text-gray-300/100")}>
+          {header3}
+        </div>
+      </div>
     </div>
   );
 };
