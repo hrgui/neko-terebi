128b70a65475117c3830c3f0aba5636e1eaff3d4
Harman Goei <5001432+hrgui@users.noreply.github.com>
2024-11-03 10:56:21 -0800

    feat: horizontal scrolling works now for collections

--- packages/components/Cell/FocusableLinkCell.tsx
+++ packages/components/Cell/FocusableLinkCell.tsx
@@ -2,12 +2,29 @@ import { useFocusable } from "@noriginmedia/norigin-spatial-navigation";
 import { Link, LinkProps } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
 import Cell, { CellProps } from "./Cell";
+import { useEffect } from "react";
 
 type Props = {
   focusKey?: string;
   focusClassName?: string;
 } & LinkProps &
   CellProps;
+
+function isInViewport(element: HTMLElement) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function isCollectionInViewport(element: HTMLElement) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+  );
+}
 
 const FocusableLinkCell = ({
   focusKey,
@@ -18,7 +35,55 @@ const FocusableLinkCell = ({
   header3,
   imageUrl,
 }: Props) => {
-  const { ref, focused } = useFocusable({ focusKey: focusKey });
+  const { ref, focused } = useFocusable({
+    focusKey: focusKey,
+    onEnterPress: () => {
+      const currentEl = ref.current as HTMLElement;
+
+      if (!currentEl) {
+        return;
+      }
+
+      currentEl.click();
+    },
+  });
+
+  useEffect(() => {
+    if (!focused) {
+      return;
+    }
+
+    const currentElement = ref.current as HTMLElement;
+    const parentEl = currentElement.parentNode as HTMLElement;
+    const rect = currentElement.getBoundingClientRect();
+
+    if (isInViewport(currentElement)) {
+      return;
+    }
+
+    if (!parentEl) {
+      return;
+    }
+
+    if (!isCollectionInViewport(parentEl)) {
+      return;
+    }
+
+    (parentEl as any).currentScrollLeftPosition = (parentEl as any).currentScrollLeftPosition ?? 0;
+
+    if (rect.x < 0) {
+      // need to add
+      (parentEl as any).currentScrollLeftPosition =
+        (parentEl as any).currentScrollLeftPosition + rect.width;
+    } else {
+      // need to subtract
+      (parentEl as any).currentScrollLeftPosition =
+        (parentEl as any).currentScrollLeftPosition - rect.width;
+    }
+
+    parentEl.style.transform = `translateX(${(parentEl as any).currentScrollLeftPosition}px)`;
+  }, [focused, ref]);
+
   return (
     <Link to={to} ref={ref} className={twMerge(className)}>
       <Cell focused={focused} {...{ header1, header2, header3, imageUrl }} />
