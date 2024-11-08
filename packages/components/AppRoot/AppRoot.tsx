0644409f7fc553e54bf2f82f7a79336f61306f11
Harman Goei <5001432+hrgui@users.noreply.github.com>
2024-11-07 20:44:34 -0800

    feat: add ScrollRestoration component for now despite it does nothing

--- packages/components/AppRoot/AppRoot.tsx
+++ packages/components/AppRoot/AppRoot.tsx
@@ -2,6 +2,7 @@ import { Outlet, useNavigation } from "react-router";
 import LoadingOverlay from "@hrgui/neko-terebi-react-component-loading";
 import AppLayout from "@hrgui/neko-terebi-react-component-app-layout";
 import { FocusContext, useFocusable } from "@noriginmedia/norigin-spatial-navigation";
+import { ScrollRestoration } from "react-router-dom";
 
 export function Root() {
   const navigation = useNavigation();
@@ -12,6 +13,7 @@ export function Root() {
       <AppLayout ref={ref}>
         {navigation.state === "loading" && <LoadingOverlay />}
         <Outlet />
+        <ScrollRestoration />
       </AppLayout>
     </FocusContext.Provider>
   );
