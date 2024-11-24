26b6d6209153ff5c0544013b0a4fc238862bb347
Harman Goei <5001432+hrgui@users.noreply.github.com>
2024-11-24 08:56:11 -0800

    feat: fix ErrorPage

--- packages/pages/ErrorPage/ErrorPage.tsx
+++ packages/pages/ErrorPage/ErrorPage.tsx
@@ -5,18 +5,33 @@ import { ErrorComponentProps } from "@tanstack/react-router";
 import { useEffect } from "react";
 
 export default function ErrorPage(props: ErrorComponentProps) {
-  const { focusSelf, ref } = useFocusable();
+  const { focusSelf, ref, focused } = useFocusable({ onEnterPress: props.reset });
 
   useEffect(() => {
     focusSelf();
   }, [focusSelf]);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <pre>{JSON.stringify(props)}</pre>
-      <button ref={ref}>OK</button>
+    <div
+      id="error-page"
+      className="overflow-hidden absolute w-full h-full @asvw:pl-[160px] @asvw:pt-[40px]"
+    >
+      <div className="text-center w-full">
+        <h1 className="text-headline-l">Oops!</h1>
+        <p className="text-cta">Sorry, an unexpected error has occurred.</p>
+        <pre>{JSON.stringify(props)}</pre>
+        <button
+          className={
+            focused
+              ? "bg-white/100 text-black/100 text-cta rounded-full @asvw:w-[628px] @asvw:p-[16px]"
+              : "bg-gray-800/100 text-white/100 text-cta rounded-full @asvw:w-[628px] @asvw:p-[16px]"
+          }
+          ref={ref}
+          onClick={props.reset}
+        >
+          Retry
+        </button>
+      </div>
     </div>
   );
 }
